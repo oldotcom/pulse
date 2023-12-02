d3db325bf17f6dcab99fd2b5c5ec8bd200d7a8da
Oleksa <oleksa.antsupov@gmail.com>
2023-12-02 17:14:32 +0200

    edit js

--- src/js/script.js
+++ src/js/script.js
@@ -104,7 +104,7 @@ $(document).ready(function(){
         }
     });
 
-    $("a[href^=#up]").click(function(){
+    $("a[href^='#up']").click(function(){
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
